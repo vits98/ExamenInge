38b9a28609b2d71cc145407d5f26d6f3f41d36e0
victor torrico sabja <victor.torrico17@gmail.com>
2020-06-16 09:49:56 -0400

    Added step

--- features/step_definitions/IngresarMonto_steps.rb
+++ features/step_definitions/IngresarMonto_steps.rb
@@ -2,8 +2,8 @@ Given('visito la pagina de ingresar dinero') do
     visit '/ingresarMonto'
 end
   
-Given('ingreso un monto {string} en el campo {string}') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
+Given('ingreso un monto {string} en el campo {string}') do |text, monto|
+    fill_in(monto, :with => text)
 end
   
 When('presiono el btn {string}') do |string|
